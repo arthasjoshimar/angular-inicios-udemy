948778f5ff01f5154a964b6dd2dee690c0e8d0fa
Jesus Callisaya <jesuscallisayasalinas@gmail.com>
2022-08-26 15:05:23 -0400

    Se adiciona services con get

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  get personajes(): Personaje[] {
+    return this.dbzService.personajes;
+  }
+
+  agregarNuevoPersonaje(personajeNuevo: Personaje) {
+    this.personajes.push(personajeNuevo);
+  }
+
+  constructor(private dbzService: DbzService) {
+  }
+}
